d59db35dff1dca9f7d0308a7f89712f40f78587a
An Chang <asc2683@gmail.com>
2021-11-19 09:25:09 -0500

    added unit test for add to cart, submit order, and view order

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
+
+afterEach(cleanup);
 
 test('renders menu header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Mr. Magoo's Sandwich Shop/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Mr. Magoo's Sandwich Shop/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test("view order", () => {
+    render(<App />);
+    const button = screen.getByText(/View Order/i);
+    fireEvent.click(button);
+    const orderTitle = screen.getByText(/My Order/i);
+    expect(orderTitle).toBeInTheDocument();
+});
+
+test("add to cart", () => {
+    render(<App />);
+    const addButton = screen.getAllByText('Add to Order')[0];
+    fireEvent.click(addButton);
+    const button = screen.getByText(/View Order/i);
+    fireEvent.click(button);
+    const quantity = screen.getByText(/Quantity: 1/i);
+    expect(quantity).toBeInTheDocument();
+});
+
+test("submit order", async () => {
+    const total = 0;
+    window.alert = jest.fn();
+    render(<App />);
+    const button = screen.getByText(/Submit Order/i);
+    fireEvent.click(button);
+    expect(window.alert).toBeCalledWith('Order Submitted: $' + total);
+    window.alert.mockClear();
 });
